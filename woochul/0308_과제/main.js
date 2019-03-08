5ffe0261303d7ec932d8ae98bbf9596944c4da87
Woochul Hyun <woochul.hyun91@gmail.com>
2019-03-08 17:54:36 +0900

    과제

---
+++ woochul/0308_과제/main.js
@@ -0,0 +1,15 @@
+function handBtn1() {
+    document.handMockup.src = "img/handMockup1.png";
+}
+
+function handBtn2() {
+    document.handMockup.src = "img/handMockup2.png";
+}
+
+function handBtn3() {
+    document.handMockup.src = "img/handMockup3.png";
+}
+
+function handBtn4() {
+    document.handMockup.src = "img/handMockup4.png";
+}
